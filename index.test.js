07e64bfae719029909b827b2bf53a5b15a02af6b
Christopher Camara <ccamara@uoguelph.ca>
2019-11-07 11:42:37 -0500

    added CI progess

--- index.test.js
+++ index.test.js
@@ -1,33 +1,33 @@
-let index = require('./index');
+const index = require('./index');
 
 test('Get username from message', () => {
-    expect(index.getUserFromMessage('calendar week <@username>')).toBe('username');  
-  });
+  expect(index.getUserFromMessage('calendar week <@username>')).toBe('username');
+});
 
 test('Get Current Date', () => {
-  expect(index.getCurrentDate()).toBeDefined();  
+  expect(index.getCurrentDate()).toBeDefined();
 });
 
 test('Days away date', () => {
-    expect(index.getDaysAwayDate(10)).toBeDefined();  
+  expect(index.getDaysAwayDate(10)).toBeDefined();
 });
 
 test('Append a zero to beginning of day', () => {
-    expect(index.dateAppendZero(5)).toBe('05');  
+  expect(index.dateAppendZero(5)).toBe('05');
 });
 
 test('Convert time from 24 hours to 12hour AM/PM from full date-time', () => {
-    expect(index.parseTimeOfEvent('2019-11-07T13:10:22')).toBe('01:10 PM');  
+  expect(index.parseTimeOfEvent('2019-11-07T13:10:22')).toBe('01:10 PM');
 });
 
 test('Convert time from 24 hours to 12hour AM/PM from full date-time', () => {
-    expect(index.parseTimeOfEvent('2019-11-07T00:30:00')).toBe('12:30 AM');  
+  expect(index.parseTimeOfEvent('2019-11-07T00:30:00')).toBe('12:30 AM');
 });
 
 test('Get weekday from index number', () => {
-    expect(index.parseWeekdayOfEvent(2)).toBe('TUES');  
+  expect(index.parseWeekdayOfEvent(2)).toBe('TUES');
 });
 
 test('Get month from index number', () => {
-    expect(index.parseMonthOfEvent(2)).toBe('March');  
+  expect(index.parseMonthOfEvent(2)).toBe('March');
 });
