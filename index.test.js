0454cfa9cdf11bae8ba74e4310911b02d719ccaa
Gio <gdiamant@uoguelph.ca>
2019-11-07 10:41:46 -0500

    Added tests and force exit for Jest

--- index.test.js
+++ index.test.js
@@ -1,10 +1,8 @@
 let index = require('./index');
 
-console.log(index);
-
-//test('Get username from message', () => {
-//    expect(index.getUserFromMessage('calendar week @username')).toBe('username');  
-//  });
+test('Get username from message', () => {
+    expect(index.getUserFromMessage('calendar week <@username>')).toBe('username');  
+  });
 
 test('Get Current Date', () => {
   expect(index.getCurrentDate()).toBeDefined();  
@@ -20,6 +18,10 @@ test('Append a zero to beginning of day', () => {
 
 test('Convert time from 24 hours to 12hour AM/PM from full date-time', () => {
     expect(index.parseTimeOfEvent('2019-11-07T13:10:22')).toBe('01:10 PM');  
+});
+
+test('Convert time from 24 hours to 12hour AM/PM from full date-time', () => {
+    expect(index.parseTimeOfEvent('2019-11-07T00:30:00')).toBe('12:30 AM');  
 });
 
 test('Get weekday from index number', () => {
