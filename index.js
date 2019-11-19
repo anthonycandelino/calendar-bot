4bd580837876753006e4c3d9798c242d3131675e
Anthony Candelino <32252982+anthonycandelino@users.noreply.github.com>
2019-11-19 14:32:24 -0500

    New test cases for calendar event handling #8618

--- index.js
+++ index.js
@@ -383,10 +383,9 @@ function parseMonthOfEvent(index) {
 /**
 * Canverts an event object into a string
 * @param {Event} events
-* @param {String} userSent
 * @return {String}
 */
-function eventsToString(events, userSent) {
+function eventsToString(events) {
   if (events.length) {
     let eventString = '';    
     let prevDayNum = -1;
@@ -402,17 +401,18 @@ function eventsToString(events, userSent) {
     });
     return eventString;
   } else {
-    return 'No upcoming events today.';
+    eventString = 'No upcoming events.';
+    console.log(eventString);
+    return eventString;
   }
 }
 
 /**
 * Finds time between events in work day
 * @param {Event} events
-* @param {String} userSent
 * @return {String}
 */
-function freeTimeToString(events, userSent) {
+function freeTimeToString(events) {
   if (events.length) {
     let eventString = '';    
     let prevDayNum = -1;
@@ -425,17 +425,17 @@ function freeTimeToString(events, userSent) {
           eventString += `${parseWeekdayOfEvent(day.getDay())} - ${parseMonthOfEvent(day.getMonth())} ${day.getDate()}\n`;
           prevDayNum = day.getDay();
         }
-        eventString += `\t\t08:00 AM - ${parseTimeOfEvent(start)} \n\t\t${parseTimeOfEvent(end)} - `;
+        eventString += `\t\t08:00 AM - ${parseTimeOfEvent(start)}\n\t\t${parseTimeOfEvent(end)} - `;
       } else {
         const start = event.start.dateTime || event.start.date;
         const end = event.end.dateTime || event.end.date;
-        eventString += `${parseTimeOfEvent(start)} \n\t\t${parseTimeOfEvent(end)} - `;
+        eventString += `${parseTimeOfEvent(start)}\n\t\t${parseTimeOfEvent(end)} - `;
       }
     });
     eventString += '04:30 PM'
     return eventString;
   } else {
-    return 'Free all day!';
+    return '\t\tYou\'re free all day!';
   }
 }
 
@@ -471,4 +471,4 @@ function eventRecipientToString(userSent, destUser, eventType = null) {
   return userString;
 }
 
-module.exports = {getUserFromMessage, getCurrentDate, getDaysAwayDate, dateAppendZero, parseTimeOfEvent, parseWeekdayOfEvent, parseMonthOfEvent};
+module.exports = {getUserFromMessage, getCurrentDate, getDaysAwayDate, dateAppendZero, parseTimeOfEvent, parseWeekdayOfEvent, parseMonthOfEvent, eventsToString, freeTimeToString, eventRecipientToString};
--- index.test.js
+++ index.test.js
@@ -1,5 +1,131 @@
 const index = require('./index');
 
+const fullDayEvent = [
+  { 
+    summary: 'Full Day Event',
+    start:{ dateTime: '2019-11-19T08:30:00-05:00' },
+    end:{ dateTime: '2019-11-19T16:30:00-05:00' },
+  }
+];
+
+const morningEvents = [ 
+  { 
+    summary: 'Morning Stand up',
+    start:{ dateTime: '2019-11-19T08:30:00-05:00' },
+    end:{ dateTime: '2019-11-19T09:30:00-05:00' },
+  },
+  { 
+    summary: 'Coffee Run',
+    start:{ dateTime: '2019-11-19T09:45:00-05:00' },
+    end:{ dateTime: '2019-11-19T10:00:00-05:00' },
+  }
+];
+
+const dayEvents = [ 
+  { 
+    summary: 'Morning Stand up',
+    start:{ dateTime: '2019-11-19T08:30:00-05:00' },
+    end:{ dateTime: '2019-11-19T09:30:00-05:00' },
+  },
+  { 
+    summary: 'Afternoon hang',
+    start:{ dateTime: '2019-11-19T12:45:00-05:00' },
+    end:{ dateTime: '2019-11-19T13:00:00-05:00' },
+  }
+];
+
+const preNoonPostNoonEvent = [  
+  {
+  summary: '1 Bite Lunch Time',
+  start:{ dateTime: '2019-11-19T11:59:00-05:00' },
+  end:{ dateTime: '2019-11-19T12:01:00-05:00' },
+  },
+];
+
+const weekEvents = [ 
+  { 
+    summary: 'Weekly Stand up',
+    start:{ dateTime: '2019-11-26T08:30:00-05:00' },
+    end:{ dateTime: '2019-11-26T09:30:00-05:00' },
+  },
+  { 
+    summary: 'Afternoon hang',
+    start:{ dateTime: '2019-11-28T12:45:00-05:00' },
+    end:{ dateTime: '2019-11-28T13:00:00-05:00' },
+  }
+];
+
+const weekIntoNewMonthEvents = [ 
+  { 
+    summary: 'End of month event',
+    start:{ dateTime: '2019-10-31T08:30:00-05:00' },
+    end:{ dateTime: '2019-10-31T09:30:00-05:00' },
+  },
+  { 
+    summary: 'Start of month event',
+    start:{ dateTime: '2019-11-01T12:45:00-05:00' },
+    end:{ dateTime: '2019-11-01T13:00:00-05:00' },
+  }
+];
+
+const emptyEventList = [];
+
+test('Get string for empty event list to string', () => {
+  expect(index.eventsToString(emptyEventList)).toBe('No upcoming events.');
+});
+
+test('Get full day event converted in string format', () => {
+  expect(index.eventsToString(fullDayEvent)).toBe('TUES - November 19\n\t\t08:30 AM - 04:30 PM: Full Day Event\n');
+});
+
+test('Get personal morning events when there are multiple morning events', () => {
+  expect(index.eventRecipientToString('self','self','morning') + index.eventsToString(morningEvents)).toBe('Your calendar for this morning:\nTUES - November 19\n\t\t08:30 AM - 09:30 AM: Morning Stand up\n\t\t09:45 AM - 10:00 AM: Coffee Run\n');
+});
+
+test('Get morning event when it stars before noon and ends after noon', () => {
+  expect(index.eventRecipientToString('self','self','morning') + index.eventsToString(preNoonPostNoonEvent)).toBe('Your calendar for this morning:\nTUES - November 19\n\t\t11:59 AM - 12:01 PM: 1 Bite Lunch Time\n');
+});
+
+test('Get different team member\'s morning events', () => {
+  expect(index.eventRecipientToString('Joe','self','morning') + index.eventsToString(morningEvents)).toBe('<@Joe>\'s calendar for this morning:\nTUES - November 19\n\t\t08:30 AM - 09:30 AM: Morning Stand up\n\t\t09:45 AM - 10:00 AM: Coffee Run\n');
+});
+
+test('Get personal day events when there are multiple events', () => {
+  expect(index.eventRecipientToString('self','self','today') + index.eventsToString(dayEvents)).toBe('Your calendar for today:\nTUES - November 19\n\t\t08:30 AM - 09:30 AM: Morning Stand up\n\t\t12:45 PM - 01:00 PM: Afternoon hang\n');
+});
+
+test('Get personal day with 1 event', () => {
+  expect(index.eventRecipientToString('self','self','today') + index.eventsToString(fullDayEvent)).toBe('Your calendar for today:\nTUES - November 19\n\t\t08:30 AM - 04:30 PM: Full Day Event\n');
+});
+
+test('Get team member\'s day events when there are multiple events', () => {
+  expect(index.eventRecipientToString('Joe','self','today') + index.eventsToString(dayEvents)).toBe('<@Joe>\'s calendar for today:\nTUES - November 19\n\t\t08:30 AM - 09:30 AM: Morning Stand up\n\t\t12:45 PM - 01:00 PM: Afternoon hang\n');
+});
+
+test('Get team member\'s day with 1 event', () => {
+  expect(index.eventRecipientToString('Joe','self','today') + index.eventsToString(fullDayEvent)).toBe('<@Joe>\'s calendar for today:\nTUES - November 19\n\t\t08:30 AM - 04:30 PM: Full Day Event\n');
+});
+
+test('Get personal week events when there are multiple events', () => {
+  expect(index.eventRecipientToString('self','self','week') + index.eventsToString(weekEvents)).toBe('Your calendar for the week:\nTUES - November 26\n\t\t08:30 AM - 09:30 AM: Weekly Stand up\nTHURS - November 28\n\t\t12:45 PM - 01:00 PM: Afternoon hang\n');
+});
+
+test('Get personal week events with 1 event', () => {
+  expect(index.eventRecipientToString('self','self','week') + index.eventsToString(preNoonPostNoonEvent)).toBe('Your calendar for the week:\nTUES - November 19\n\t\t11:59 AM - 12:01 PM: 1 Bite Lunch Time\n');
+});
+
+test('Get team member\'s week events when there are multiple events', () => {
+  expect(index.eventRecipientToString('Joe','self','week') + index.eventsToString(weekEvents)).toBe('<@Joe>\'s calendar for the week:\nTUES - November 26\n\t\t08:30 AM - 09:30 AM: Weekly Stand up\nTHURS - November 28\n\t\t12:45 PM - 01:00 PM: Afternoon hang\n');
+});
+
+test('Get team member\'s week events with 1 event', () => {
+  expect(index.eventRecipientToString('Joe','self','week') + index.eventsToString(preNoonPostNoonEvent)).toBe('<@Joe>\'s calendar for the week:\nTUES - November 19\n\t\t11:59 AM - 12:01 PM: 1 Bite Lunch Time\n');
+});
+
+test('Get week events when weekdays carry over into a different month', () => {
+  expect(index.eventsToString(weekIntoNewMonthEvents)).toBe('THURS - October 31\n\t\t09:30 AM - 10:30 AM: End of month event\nFRI - November 1\n\t\t01:45 PM - 02:00 PM: Start of month event\n');
+});
+
 test('Get username from message', () => {
   expect(index.getUserFromMessage('calendar week <@username>')).toBe('username');
 });
